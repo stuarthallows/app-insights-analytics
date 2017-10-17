78a7a96f167861a99bcaedc12f1b1ca661f2a316
Stuart Hallows <stuart.hallows@gmail.com>
2017-10-18 09:04:30 +1030

    initial commit

---
+++ analytics.fs
@@ -0,0 +1,93 @@
+// notes from advanced analytics video
+// how to add custom events, metrics and counters?
+
+traces | project timestamp, message | take 10
+
+traces | take 10
+
+// how many records across all tables
+union * | count 
+
+// how many records in traces tables
+traces | count 
+
+// search all columns
+traces | where * has "error" | take 10 
+
+// project, extend and split
+traces 
+| where cloud_RoleInstance startswith 'AiConnect'
+| project cloud_RoleInstance
+| extend machineId = split(cloud_RoleInstance, "-")[1]
+| take 10
+
+// error distribution
+traces
+| where timestamp > ago(1d) 
+| extend ok = severityLevel <= 1
+| summarize count() by ok
+
+// ok / error by time
+traces
+| where timestamp > ago(7d) 
+| summarize ok = count(severityLevel <= 2), error = count(severityLevel > 2) by bin(timestamp, 1h) 
+| render timechart 
+
+// % ok by time
+traces
+| where timestamp > ago(7d) 
+| summarize ok = count(severityLevel <= 2), error = count(severityLevel > 2), totalCount = count() by bin(timestamp, 1h) 
+| extend pctOk = ok * 100.00 / totalCount 
+| project pctOk, timestamp 
+| render timechart 
+
+// percentiles
+pageViews
+| where timestamp > ago(7d) 
+| summarize percentiles(duration, 50, 95) , count() by bin(timestamp, 1h)
+| render timechart 
+
+// most recent message per device
+traces
+| where timestamp > ago(3d) 
+| where notempty(cloud_RoleInstance)
+| where severityLevel >= 1
+| summarize argmax(timestamp, message, severityLevel) by cloud_RoleInstance
+| sort by cloud_RoleInstance asc
+
+// count message
+traces
+| where timestamp > ago(1d) 
+| where customDimensions['Application'] == "Ai.Connect.UI"
+| summarize count() by message
+| order by count_ desc 
+| take 20
+
+// count messages with reduce by to group by message type
+traces
+| where timestamp > ago(1d) 
+| where customDimensions['Application'] == "Ai.Connect.UI"
+| summarize count() by message
+| reduce by message 
+| order by Count desc 
+| take 20
+
+// autocluster heuristically finds a small number of clusters with similar rows
+traces
+| where timestamp > ago(1d) 
+| project message 
+| evaluate autocluster_v2()
+
+// basket to find frequent patterns
+traces
+| where timestamp > ago(1d) 
+| where severityLevel > 2
+| evaluate basket()
+
+// patterns of messages between Australia and RoTW
+traces
+| where timestamp > ago(2d) 
+| extend isAustralia = iff(client_CountryOrRegion == "Australia", "T", "F")
+| project message , isAustralia
+| evaluate diffpatterns(isAustralia, "T", "F")
+
